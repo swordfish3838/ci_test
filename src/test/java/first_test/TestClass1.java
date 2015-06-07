043e2fdf87dc08d104791e8d0ed53cbf3f0129cf
swordfish3838 <swordfish3838@gmail.com>
2015-06-07 13:17:45 +0800

    developer1 committed this

--- src/test/java/first_test/TestClass1.java
+++ src/test/java/first_test/TestClass1.java
@@ -4,20 +4,14 @@ public class TestClass1 {
 
 	
 	public void thisIsDev2(){
-		
+		//this is from developer1
+		int x = 0;
+		System.out.println("X is : " + x);
+				
 	}
 
 	public void thisIsDev1(){
 		
 	}
 	
-	public void thisIsDev1With2(){
-	}
-	public void thisIsDev2Change2(){
-		
-	}
-	
-	public void thisIsToAddStory2(){
-		
-	}
 }
